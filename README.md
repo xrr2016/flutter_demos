6fc92d9ccacc79de18000d8cf701883593984026
Cold Stone <xiaoranran1993@outlook.com>
2021-08-02 11:09:56 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flutter 示例集合
+
+实现 Flutter 应用开发基础功能
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import './home/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter 示例集合',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
